97c96d83a7060d79bf9adb41fdd1fd581898272c
haoxin <coderhaoxin@outlook.com>
2015-11-04 18:39:26 +0800

    add cors

--- node/app.js
+++ node/app.js
@@ -1,16 +1,29 @@
 
+'use strict'
+
 const request = require('requisition')
 const app = require('koa')()
 
+app.use(function*(next) {
+  this.set('Access-Control-Allow-Credentials', 'true')
+  this.set('Access-Control-Allow-Origin', 'some-origin')
+  this.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,others')
+  this.set('Access-Control-Allow-Headers', 'Accept,Content-Type,Origin,X-Requested-With,others')
+
+  yield next
+})
+
 app.use(function*() {
-  const url = decodeURIComponent(this.query.url)
+  let url = this.query.url
   if (!url) {
     return
   }
 
+  url = decodeURIComponent(url)
   const res = yield request(url)
   this.body = yield res.text()
 })
 
 app.listen(3003)
+
 console.info('listen: %d', 3003)
